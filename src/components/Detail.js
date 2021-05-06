05fc4f5c7303d8c36384d5469547c138eede730c
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-07 00:39:47 +0100

    add db call for detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import styled from 'styled-components';
+import db from '../firebase';
 
 function Detail() {
+  const { id } = useParams();
+  const [detailData, setDetailData] = useState({});
+
+  useEffect(() => {
+    db.collection('movies')
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setDetailData(doc.data());
+        } else {
+          console.log('no such document in firebase 🔥');
+        }
+      })
+      .catch((error) => {
+        console.log('Error getting document:', error);
+      });
+  }, [id]);
+
   return (
     <Container>
       <Background>
-        <img alt="" src="" />
+        <img alt={detailData.title} src={detailData.backgroundImg} />
       </Background>
-      Detail
+
+      <ImageTitle>
+        <img alt={detailData.title} src={detailData.titleImg} />
+      </ImageTitle>
+      <ContentMeta>
+        <Controls>
+          <Player>
+            <img src="/images/play-icon-black.png" alt="" />
+            <span>Play</span>
+          </Player>
+          <Trailer>
+            <img src="/images/play-icon-white.png" alt="" />
+            <span>Trailer</span>
+          </Trailer>
+          <AddList>
+            <span />
+            <span />
+          </AddList>
+          <GroupWatch>
+            <div>
+              <img src="/images/group-icon.png" alt="" />
+            </div>
+          </GroupWatch>
+        </Controls>
+        <SubTitle>{detailData.subTitle}</SubTitle>
+        <Description>{detailData.description}</Description>
+      </ContentMeta>
     </Container>
   );
 }
